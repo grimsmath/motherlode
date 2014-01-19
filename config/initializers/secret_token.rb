d481b1e5a51d41eac4e434fa7af0d62dae35dcb3
Matt Walston <m@mlw.ac>
2014-01-19 13:47:08 -0500

    Initial commit of new rails project sans active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotherlodeMongo::Application.config.secret_key_base = '8ddff765135ca963bf02314f19fa452c59ba4293da78ac69ecbc9317967b8c21c62eaccefd7686fe0c030a89c47240820f2b29ec304a9a1afd024c4cf3440ad9'
