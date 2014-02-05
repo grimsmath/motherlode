ab53b60626c2d024685ba3a0b768e9a8122cd370
Matt Walston <m@mlw.ac>
2014-02-05 18:19:42 -0500

    Installed pry

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,9 @@ Motherlode::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  silence_warnings do
+    require 'pry-remote'
+  end
+  
 end
