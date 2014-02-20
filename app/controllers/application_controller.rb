c6fecf395249c98660f2b926702baf97f0eb354c
Matt Walston <m@mlw.ac>
2014-02-19 23:26:37 -0500

    Added basic pundit code for ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Include Pundit module for authorization
-  include Pundit
+  include Pundit # For authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:error] = 'You are not authorized to perform this action.'
+    redirect_to request.header['Referer'] || root_path
+  end
 end
