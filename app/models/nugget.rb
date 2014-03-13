86b6e67b332648728c7fe892753cb23d3467ca5a
David King <grimsmath@gmail.com>
2014-03-12 22:56:18 -0400

    fixing styles

--- app/models/nugget.rb
+++ app/models/nugget.rb
@@ -3,12 +3,15 @@ class Nugget
   include Mongoid::Tree
   belongs_to :category
   belongs_to :user
-  embeds_many :contents, cascade_callbacks: true
+
+  #embeds_many :contents, cascade_callbacks: true
+  #accepts_nested_attributes_for :contents, :allow_destroy => true
+
   embeds_many :images, cascade_callbacks: true
-  accepts_nested_attributes_for :contents, :allow_destroy => true
   accepts_nested_attributes_for :images, :allow_destroy => true
 
   field :title, type: String
+  field :content, type: String
   field :approved, type: Boolean
 
   # All nugget content is stored in an array of embedded documents which are persisted Content entities.
