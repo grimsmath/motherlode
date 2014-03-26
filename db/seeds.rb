c6b145cce52d69af408e4b2220f91c153d6bc1b3
Matt Walston <m@mlw.ac>
2014-03-26 12:19:54 -0400

    Changed seed comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# This seed data provides a default administrator user and optional seed data. When seeding a production deployment,
+# change the default administrator credentials to a temporary yet secure selection. Once deployed, change password.
+# It is recommend that example data be disabled prior to deployment.
 
 # =============================================
 # Users
