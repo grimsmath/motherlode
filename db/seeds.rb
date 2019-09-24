ec902b22983cde43a2fb52ce44e933f5024b6140
David King <davidk@summitfinances.com>
2019-09-23 20:53:15 -0400

    Updated mongoid config

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,7 @@ User.destroy_all()
 # =============================================
 # Users
 # =============================================
-User.create(email: 'david.king@unf.edu', name: 'David King', password: 'password', admin: true)
-User.create(email: 'geoff.wark@unf.edu', name: 'Geoff Wark', password: 'password', admin: true)
-User.create(email: 'm@mlw.ac', name: 'Matt Walston', password: 'password', admin: true)
-User.create(email: 'nikideroin@gmail.com', name: 'Nicole DeRoin', password: 'password', admin: true)
+User.create(email: 'grimsmath@gmail.com', name: 'David King', password: 'password', admin: true)
 
 # remove all the old categories from the database
 Category.destroy_all()
